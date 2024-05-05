eef2754472b024f24420960480155da3d8dac266
ybouaouni <yacinebouaouni07@gmail.com>
2024-05-06 00:37:37 +0200

    add dataset

---
+++ audiodetect/data/__init__.py
@@ -0,0 +1,3 @@
+from .dataset import AudioDataset
+
+__all__ = ['AudioDataset']
--- audiodetect/data/utils_data.py
+++ audiodetect/data/utils_data.py
@@ -5,13 +5,16 @@
 
 Requires librosa library for audio processing tasks.
 """
+
 import librosa
 import numpy as np
+
 
 def load_audio_file(file_path):
     """load audio from file_path"""
     audio, _ = librosa.load(file_path, sr=None)
     return audio
+
 
 def compute_log_mel_spectrogram(audio, config):
     """
@@ -33,13 +36,15 @@ def compute_log_mel_spectrogram(audio, config):
 
     """
     # Compute Mel spectrogram
-    mel_spectrogram = librosa.feature.melspectrogram(y=audio,
-                                                     sr=config.FS,
-                                                     n_fft=config.N_FFT,
-                                                     hop_length=config.HOP,
-                                                     n_mels=config.N_MELS,
-                                                     fmin=config.MIN_FREQ,
-                                                     fmax=config.MAX_FREQ)
+    mel_spectrogram = librosa.feature.melspectrogram(
+        y=audio,
+        sr=config.FS,
+        n_fft=config.N_FFT,
+        hop_length=config.HOP,
+        n_mels=config.N_MELS,
+        fmin=config.MIN_FREQ,
+        fmax=config.MAX_FREQ,
+    )
     # Compute log Mel spectrogram
     log_mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
     return log_mel_spectrogram
