7f7ca646854834ad612145a53c3136afe55b3c72
ybouaouni <yacinebouaouni07@gmail.com>
2024-05-07 02:22:46 +0200

    add basic test for dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,10 +1,20 @@
 from audiodetect.data import AudioDataset
 from omegaconf import OmegaConf
+import pandas as pd
+import os
+import json
 
 config = OmegaConf.load('../config/config.yaml')
-dataset = AudioDataset(config=config)
+data_df = pd.read_csv(f'{config.DATA_ROOT}/train_metadata.csv')
+train_df = data_df[['primary_label', 'rating', 'filename']].copy()
+label2id = json.load(open(os.path.join(config.DATA_ROOT, "label2id.json")))
+
+
+train_df['target'] = train_df.primary_label.map(label2id)
+train_df['filepath'] = config.PREPROCESSED_DATA_ROOT + '/train_audio/' + train_df.filename.str.replace('ogg', 'npy')
+train_df['samplename'] = train_df.filename.map(lambda x: x.split('/')[0] + '-' + x.split('/')[-1].split('.')[0])
+
+dataset = AudioDataset(config=config, data_df=train_df)
 
 print(len(dataset))
-print(dataset.id2label)
-print(dataset.label2id)
 print(dataset[0])
